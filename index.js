67c305d616076b78c59662757216d6603422db93
lndy <gmperez4@up.edu.ph>
2025-02-27 15:09:29 +0800

    UPDATE: Added functions

---
+++ index.js
@@ -0,0 +1,37 @@
+import { appendFileSync } from 'node:fs';
+import { v4 as uuidv4 } from 'uuid';
+import validator from 'validator';
+
+function generateUniqueID(firstName, lastName){
+// if not fname and lname return null
+    if(!firstName || !lastName) return null;
+// convert first letter of fname to lowercase
+    const fname = firstName.charAt(0).toLowerCase();
+// convert last name to lowercase
+    const lname = lastName.toLowerCase();
+// unique string
+    const uniqueString = uuidv4().replace(/-/g," ").substring(0,8); //https://stackoverflow.com/questions/20994768/how-to-reduce-length-of-uuid-generated-using-randomuuid
+// return fname, lname, and string
+    return `${fname}${lname}${uniqueString}`;
+}
+
+function addAccount(user){
+// if user length not 4 return false
+    if(user.length != 4) return false;
+
+    const[firstName, lastName, email, age] = user;
+// if not fname, lname, email & age<18, return false
+    if(!firstName || !lastName || !email || age<18) return false;
+// if email is in valid format
+    if(!validator.isEmail(email)) return false;
+// generate unique ID
+    const uniqueID = generateUniqueID(firstName,lastName);
+
+    const userAdd = `${firstName},${lastName},${email},${age},${uniqueID}\n`;
+
+// append
+    appendFileSync('users.txt', userAdd , 'utf-8');
+    return true;
+}
+
+export{ generateUniqueID, addAccount };
---
+++ test.js
@@ -0,0 +1,7 @@
+import { generateUniqueID, addAccount } from "./index.js";
+
+console.log(addAccount(['Alan', 'Turing', 'aturing@w3c.com', 58]));
+console.log(addAccount(['Tim', 'Berners-Lee', 'tim@w3c.com', 25]));
+console.log(addAccount(['Ted', 'Nelson', 'ted.n@w3c.com', 43]));
+console.log(addAccount(['Baby', 'Cutie', 'baby.n@w3c.com', 3]));
+console.log(addAccount(['Incomplete', 'baby.n@w3c.com', 3]));
